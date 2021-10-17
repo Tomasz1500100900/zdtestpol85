46204d2796d421b39d28356bc79f8d9e7f8803bc
Tomasz <tpreiniger@gmail.com>
2021-10-17 14:34:53 +0200

    cammit 3

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int liczba;
+        liczba = 5;
+        int liczba2 = 7;
+        System.out.println(liczba2);
+    }
+}
