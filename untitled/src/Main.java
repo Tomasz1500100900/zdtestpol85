ef8e8249425edf08b75c1d815ed5f235cba90787
Tomasz <tpreiniger@gmail.com>
2021-11-07 15:04:06 +0100

    #dobrazmiana

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,31 +1,52 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int liczba;
-        liczba = 5;int liczba2 = 7;
-        System.out.println("Hello World" + liczba);
+//        int liczba = 5;
+//        int liczba2 = 5;
+//        System.out.println("Hello World" + liczba);
+//
+//
+//      System.out.println(liczba2);
+//
+//        String name = "Tomasz";
+//        int wiek = 28;
+//        System.out.println("My name is " + name + " and I'm " + wiek);
+//
+//        String account = "TomaszPreiniger";
+//        int dlugosc = account.length();
+//        System.out.println(dlugosc);
+//
+//        System.out.println(account.indexOf("rr"));
+//
+//        System.out.println(account.substring(0,6));
+//
+//        char a = '1';
+//        char alpha = '\u0381';
+//        System.out.println(alpha);
+//
+//        char[] witaj = {'w', 'i', 't', 'a', 'j'};
+//        System.out.println(alpha);
+//        Character.isDigit(a);
+//        System.out.println(Character.isDigit(a));
+//
+//        if (liczba>liczba2 & liczba==liczba2) {
+//            System.out.println("Program dziala");
+//        }else {
+//            System.out.println("Program nie dziala");
+//            if (liczba == liczba2) {
+//                System.out.println("Program dziala tez");
+//            }
+//        }
+        Scanner skaner = new Scanner(System.in);
+        System.out.println("Wpisz swoje imie");
+        String userInputName = skaner.nextLine();
+        System.out.println("Wpisz swoje nazwisko");
+        String userInputSurname = skaner.nextLine();
+        System.out.println("Witaj " + userInputName +" " + userInputSurname + "!");
 
 
-      System.out.println(liczba2);
 
-        String name = "Tomasz";
-        int wiek = 28;
-        System.out.println("My name is " + name + " and I'm " + wiek);
 
-        String account = "TomaszPreiniger";
-        int dlugosc = account.length();
-        System.out.println(dlugosc);
-
-        System.out.println(account.indexOf("rr"));
-
-        System.out.println(account.substring(0,6));
-
-        char a = 'a';
-        char alpha = '\u0381';
-        System.out.println(alpha);
-
-        char[] witaj = {'w', 'i', 't', 'a', 'j'};
-        System.out.println(witaj);
-        Character.isDigit(a);
-        System.out.println(Character.isDigit(liczba));
     }
 }
