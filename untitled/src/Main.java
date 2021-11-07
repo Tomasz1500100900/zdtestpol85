c836d75a8da9b1df59aa64c8eae3c2aca2f61b3f
Tomasz <tpreiniger@gmail.com>
2021-11-07 12:58:45 +0100

    Merge remote-tracking branch 'origin/master'

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-
         int liczba;
         liczba = 5;
         int liczba2 = 7;
+        System.out.println("Hello World" + liczba);
+
+
         System.out.println(liczba2);
+
+        String name = "Tomasz";
+        int wiek = 28;
+        System.out.println("My name is " + name + " and I'm " + wiek);
     }
 }
